4bab4742681b9cf1eaee23add059d797223ae582
Matheus2509 <matheusmota265@gmail.com>
2024-10-12 20:53:16 -0300

    1.5.0
    Atualização mobile e adição de habilidades.

--- script.js
+++ script.js
@@ -16,38 +16,3 @@ function proximaImg(){
 
     document.getElementById('radio'+cont).checked = true        
 }
-
-/*Audios*/
-
-var audio = document.getElementById('backgroundMusic');
-
-        // Verifica se o autoplay foi bloqueado
-        document.addEventListener('DOMContentLoaded', function() {
-            var playPromise = audio.play();
-
-            if (playPromise !== undefined) {
-                playPromise.then(function() {
-                    // O áudio começou a tocar automaticamente
-                }).catch(function(error) {
-                    // Autoplay bloqueado, adicionando botão de play.
-                    console.log('Autoplay bloqueado: adicionando botão de play.');
-                    var button = document.createElement('button');
-                    button.innerHTML = "Reproduzir Música";
-                    button.style.fontSize = '18px';
-                    button.style.padding = '10px 20px';
-                    button.style.marginTop = '20px';
-
-                    // Evento para clique e toque
-                    button.addEventListener('click', playAudio);
-                    button.addEventListener('touchstart', playAudio);
-
-                    // Função para iniciar o áudio
-                    function playAudio() {
-                        audio.play();
-                        button.remove(); // Remove o botão após o toque/clique
-                    }
-
-                    document.body.appendChild(button);
-                });
-            }
-});
